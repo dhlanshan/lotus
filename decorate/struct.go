367e21feecec83529bb047d6ef38c80395a15a31
zhangjk <zjkmmd@163.com>
2025-01-22 10:29:42 +0800

    feat: 新增-NewStruct
    使用该函数创建的struct会自动执行struct下的postInit函数,目的是自动完成一个struct创建后的初始化操作

---
+++ decorate/struct.go
@@ -0,0 +1,12 @@
+package decorate
+
+// DataClasses 定义一个接口，包含要执行的方法
+type DataClasses interface {
+	postInit()
+}
+
+// NewStruct 包装一个带有初始化操作的struct
+func NewStruct[T DataClasses](obj T) T {
+	obj.postInit()
+	return obj
+}
