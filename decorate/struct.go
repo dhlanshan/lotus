b9169f51030e8009577e69fb80c28b12c910bf02
zjk <zjkmmd@163.com>
2025-06-27 18:34:49 +0800

    feat: 新增: 生成唯一ID的各个方法

--- decorate/struct.go
+++ decorate/struct.go
@@ -1,5 +1,7 @@
 package decorate
 
+import "fmt"
+
 // DataClasses 定义一个接口，包含要执行的方法
 type DataClasses interface {
 	postInit()
@@ -8,5 +10,6 @@ type DataClasses interface {
 // NewStruct 包装一个带有初始化操作的struct
 func NewStruct[T DataClasses](obj T) T {
 	obj.postInit()
+	fmt.Println("1")
 	return obj
 }
